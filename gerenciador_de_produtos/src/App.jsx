3e68e03ed9301997d8f9ef56c9414abcf40f3a2e
Mateus Andre Paiva de Araujo <130513304+MateusPaiva74@users.noreply.github.com>
2024-08-30 14:56:24 -0300

    testando

--- gerenciador_de_produtos/src/App.jsx
+++ gerenciador_de_produtos/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Routes} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import FornecedorList from './pages/Fornecedor/FornecedorList'
+import Navbar from './components/Navbar'
+import FornecedorForm from './pages/Fornecedor/FornecedorForm'
 
 const App = () => {
-
   return (
-
     <BrowserRouter>
-      <Routes>
-        <Route path="/"></Route>
+    <Navbar />
+    <div className="container">
+    <Routes>
+        <Route path="/" element={<Inicial />} />
+        <Route path="/listar-fornecedores" element={<FornecedorList />} />
+        <Route path="/add-fornecedores" element={<FornecedorForm />} />
+        <Route path="/edit-fornecedores/:id" element={<FornecedorForm />} />
+        <Route path="/listar-produtos" element={<ProdutoList />} />
       </Routes>
+    </div>
     </BrowserRouter>
   )
 }
