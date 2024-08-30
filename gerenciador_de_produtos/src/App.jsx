17d0292c1dce7c911f750b4645ebbfcb3eebbdaa
Mateus Andre Paiva de Araujo <130513304+MateusPaiva74@users.noreply.github.com>
2024-08-30 10:07:01 -0300

    andamento

--- gerenciador_de_produtos/src/App.jsx
+++ gerenciador_de_produtos/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes} from 'react-router-dom'
 
 const App = () => {
+
   return (
-    <div>
-      
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
